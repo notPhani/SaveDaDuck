92d016aa25150c2f18c37dbb45267e0b84bd1ca5
notPhani <phaniexplains14@gmail.com>
2025-08-21 01:33:48 +0530

    Refactor ad coordinate generation: import utility functions and initialize realAdCoords and fakeAdCoords with client dimensions

--- static/script.js
+++ static/script.js
@@ -1,3 +1,11 @@
-import {} from './utils.js';
+import { randReAds, randImAds } from './utils.js';
 
-let realAdCoords = randReAds(clientWidth)
+const bigAhhDiv = document.getElementById("bigAhhDiv");
+let clientWidth_ = bigAhhDiv.clientWidth;
+let clientHeight_ = bigAhhDiv.clientHeight;
+
+let realAdCoords = randReAds(clientWidth_, clientHeight_, 200, 200, 20);
+console.log(realAdCoords);
+
+let fakeAdCoords = randImAds(clientWidth_, clientHeight_, 200, 200, 80);
+console.log(fakeAdCoords);
